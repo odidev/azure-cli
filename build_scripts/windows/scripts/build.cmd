980dcf1953f9cccd84a48473b617223aee777f48
jiasli <4003950+jiasli@users.noreply.github.com>
2022-03-22 16:04:12 +0800

    msi python

--- build_scripts/windows/scripts/build.cmd
+++ build_scripts/windows/scripts/build.cmd
@@ -9,11 +9,14 @@ if "%CLI_VERSION%"=="" (
     echo Please set the CLI_VERSION environment variable, e.g. 2.0.13
     goto ERROR
 )
-set PYTHON_VERSION=3.8.9
+set PYTHON_VERSION=3.10.3
 
 set WIX_DOWNLOAD_URL="https://azurecliprod.blob.core.windows.net/msi/wix310-binaries-mirror.zip"
-set PYTHON_DOWNLOAD_URL="https://azurecliprod.blob.core.windows.net/util/Python389-32.zip"
+set PYTHON_DOWNLOAD_URL="https://www.python.org/ftp/python/3.10.3/python-3.10.3-embed-win32.zip"
 set PROPAGATE_ENV_CHANGE_DOWNLOAD_URL="https://azurecliprod.blob.core.windows.net/util/propagate_env_change.zip"
+
+:: https://pip.pypa.io/en/stable/installation/#get-pip-py
+set GET_PIP_DOWNLOAD_URL="https://bootstrap.pypa.io/get-pip.py"
 
 :: Set up the output directory and temp. directories
 echo Cleaning previous build artifacts...
@@ -26,7 +29,7 @@ mkdir %ARTIFACTS_DIR%
 set TEMP_SCRATCH_FOLDER=%ARTIFACTS_DIR%\cli_scratch
 set BUILDING_DIR=%ARTIFACTS_DIR%\cli
 set WIX_DIR=%ARTIFACTS_DIR%\wix
-set PYTHON_DIR=%ARTIFACTS_DIR%\Python389-32
+set PYTHON_DIR=%ARTIFACTS_DIR%\Python
 set PROPAGATE_ENV_CHANGE_DIR=%~dp0..\propagate_env_change
 
 set REPO_ROOT=%~dp0..\..\..
@@ -59,7 +62,7 @@ if not exist %WIX_DIR% (
     mkdir %WIX_DIR%
     pushd %WIX_DIR%
     echo Downloading Wix.
-    curl -o wix-archive.zip %WIX_DOWNLOAD_URL% -k
+    curl -o wix-archive.zip %WIX_DOWNLOAD_URL%
     unzip -q wix-archive.zip
     if %errorlevel% neq 0 goto ERROR
     del wix-archive.zip
@@ -72,14 +75,29 @@ if exist %PYTHON_DIR% (
     echo Using existing Python at %PYTHON_DIR%
 )
 if not exist %PYTHON_DIR% (
+    echo Setting up Python and pip
     mkdir %PYTHON_DIR%
     pushd %PYTHON_DIR%
-    echo Downloading Python.
-    curl -o Python389-32.zip %PYTHON_DOWNLOAD_URL% -k
-    unzip -q Python389-32.zip
+
+    echo Downloading Python
+    curl -o python-archive.zip %PYTHON_DOWNLOAD_URL%
+    unzip -q python-archive.zip
     if %errorlevel% neq 0 goto ERROR
-    del Python389-32.zip
+    del python-archive.zip
     echo Python downloaded and extracted successfully.
+
+    echo Installing pip
+    curl -o get-pip.py %GET_PIP_DOWNLOAD_URL%
+    %PYTHON_DIR%\python.exe get-pip.py
+    del get-pip.py
+    echo Pip set up successful.
+
+    :: Delete _pth file so that Lib\site-packages is included in sys.path
+    :: https://github.com/pypa/pip/issues/4207#issuecomment-297396913
+    :: https://docs.python.org/3.10/using/windows.html#finding-modules
+    del python310._pth
+    dir .
+
     popd
 )
 set PYTHON_EXE=%PYTHON_DIR%\python.exe
